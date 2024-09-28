895c65a8038eb66b0069afde3a7a5b86315f7207
Huỳnh Thạnh Phú <phuhynhpen@gmail.com>
2024-09-28 13:34:41 +0700

    HIUBOOK

---
+++ resources/views/layout_index/footer.blade.php
@@ -0,0 +1,129 @@
+<footer id="tg-footer" class="tg-footer tg-haslayout">
+    <div class="tg-footerarea">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <ul class="tg-clientservices">
+                        <li class="tg-devlivery">
+                            <span class="tg-clientserviceicon"><i class="icon-rocket"></i></span>
+                            <div class="tg-titlesubtitle">
+                                <h3>Miễn phí</h3>
+                                <p>Vận chuyển toàn quốc</p>
+                            </div>
+                        </li>
+                        <li class="tg-discount">
+                            <span class="tg-clientserviceicon"><i class="icon-tag"></i></span>
+                            <div class="tg-titlesubtitle">
+                                <h3>Mặt hàng</h3>
+                                <p>Đa dạng chất lượng</p>
+                            </div>
+                        </li>
+                        <li class="tg-quality">
+                            <span class="tg-clientserviceicon"><i class="icon-leaf"></i></span>
+                            <div class="tg-titlesubtitle">
+                                <h3>Chi nhánh</h3>
+                                <p>Trên cả nước</p>
+                            </div>
+                        </li>
+                        <li class="tg-support">
+                            <span class="tg-clientserviceicon"><i class="icon-heart"></i></span>
+                            <div class="tg-titlesubtitle">
+                                <h3>Hổ trợ 24/7</h3>
+                                <p>Mọi lúc / Mọi nơi</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="tg-threecolumns">
+                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                        <div class="tg-footercol">
+                            <strong class="tg-logo"><a href="javascript:void(0);"><img src="images/logoo.jpg"
+                                        alt="image description"></a></strong>
+                            <ul class="tg-contactinfo">
+                                <li>
+                                    <i class="icon-apartment"></i>
+                                    <address>140/11 Thanh Đa</address>
+                                </li>
+                                <li>
+                                    <i class="icon-phone-handset"></i>
+                                    <span>
+                                        <em>0375 - 9480 - 222</em>
+                                    </span>
+                                </li>
+                                <li>
+                                    <i class="icon-clock"></i>
+                                    <span>Mở cửa từ Thứ 2 đến thứ 7</span>
+                                </li>
+                                <li>
+                                    <i class="icon-envelope"></i>
+                                    <span>
+                                        <em><a href="mailto:support@domain.com">bookstore@gmail.com</a></em>
+                                    </span>
+                                </li>
+                            </ul>
+                            <ul class="tg-socialicons">
+                                <li class="tg-facebook"><a href="https://www.facebook.com/websitebookstore/"><i
+                                            class="fa fa-facebook"></i></a></li>
+                                <li class="tg-twitter"><a href="javascript:void(0);"><i class="fa fa-twitter"></i></a>
+                                </li>
+                                <li class="tg-linkedin"><a href="javascript:void(0);"><i class="fa fa-linkedin"></i></a>
+                                </li>
+                                <li class="tg-googleplus"><a href="javascript:void(0);"><i
+                                            class="fa fa-google-plus"></i></a></li>
+                                <li class="tg-rss"><a href="javascript:void(0);"><i class="fa fa-rss"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                        <div class="tg-footercol tg-widget tg-widgetnavigation">
+                            <div class="tg-widgettitle">
+                                <h3>Trợ giúp</h3>
+                            </div>
+                            <div class="tg-widgetcontent" style="width: 400px;">
+                                <ul>
+                                    <li><a href="javascript:void(0);">Chính sách đổi - trả - hoàn tiền</a></li>
+                                    <li><a href="javascript:void(0);">Phương thức vận chuyển</a></li>
+                                    <li><a href="javascript:void(0);">Phương thức thanh toán và xuất HĐ</a></li>
+                                    <li><a href="javascript:void(0);">Bảo Mật</a></li>
+                                    <li><a href="javascript:void(0);">Điều khoản sử dụng</a></li>
+                                    <li><a href="javascript:void(0);">Liên hệ</a></li>
+
+                                </ul>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+                        <div class="tg-footercol tg-widget tg-widgettopsellingauthors">
+                            <div class="tg-widgettitle">
+                                <h3>Nhà Sáng Lập</h3>
+                            </div>
+                            <div class="tg-widgetcontent">
+                                <ul>
+                                    <li>
+                                        <figure><a href="javascript:void(0);"><img style="width:70px;height:70px ; "
+                                                    src="images/author/leader.png" alt="image description"></a></figure>
+                                        <div class="tg-authornamebooks">
+                                            <h4><a href="javascript:void(0);">Huỳnh Thạnh Phú</a></h4>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="tg-footerbar">
+        <a id="tg-btnbacktotop" class="tg-btnbacktotop" href="javascript:void(0);"><i class="icon-chevron-up"></i></a>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <span class="tg-copyright">2021 All Rights Create By &copy; ThanhPhu</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
